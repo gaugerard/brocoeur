4fc58dba3728e3b6ae19b7cc251e9470c7955661
bapierre <pierre.baptiste@outlook.com>
2022-10-05 20:55:12 +0200

    Nerima Spring MVC + get mapping test

--- nerima/src/main/java/brocoeur/example/Main.java
+++ nerima/src/main/java/brocoeur/example/Main.java
@@ -1,10 +1,11 @@
 package brocoeur.example;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(Main.class,args);
     }
 }
--- nerima/src/main/java/brocoeur/example/controller/NerimaController.java
+++ nerima/src/main/java/brocoeur/example/controller/NerimaController.java
@@ -4,23 +4,31 @@ import brocoeur.example.service.GamePlay;
 import brocoeur.example.service.GameStrategy;
 import brocoeur.example.service.GameStrategyTypes;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
 
 @Controller
 public class NerimaController {
 
     @PostMapping("/play")
-    public HttpStatus postPlay(@RequestBody final ServiceRequest serviceRequest) {
+    public ResponseEntity<ServiceRequest> postPlay(@RequestBody final ServiceRequest serviceRequest) {
         final String userId = serviceRequest.userId();
         final GameStrategyTypes gameStrategyTypes = serviceRequest.gameStrategyTypes();
         final GameStrategy gameStrategy = gameStrategyTypes.getGameStrategy();
 
         final GamePlay gamePlay = gameStrategy.play();
         System.out.println(userId + " used strategy: " + gameStrategy + " and will play: " + gamePlay);
-        return HttpStatus.OK;
+        return new ResponseEntity<>(serviceRequest, HttpStatus.OK);
     }
 
+    @GetMapping("/")
+    public ResponseEntity<ServiceRequest> getRandomCoin() {
+        final ServiceRequest serviceRequest = new ServiceRequest("4", GameStrategyTypes.COIN_TOSS_RANDOM);
+        return new ResponseEntity<>(serviceRequest, HttpStatus.OK);
+    }
 
 }
