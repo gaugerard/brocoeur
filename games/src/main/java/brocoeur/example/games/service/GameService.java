7b20dd890567c6dd3629fc29bb7c5af7291c29be
gaugerard <gauthier.gerard@student.unamur.be>
2022-12-04 20:19:27 +0100

    test 8

--- games/src/main/java/brocoeur/example/games/service/GameService.java
+++ games/src/main/java/brocoeur/example/games/service/GameService.java
@@ -20,4 +20,12 @@ public class GameService {
             case ROULETTE -> rouletteService.play();
         };
     }
+
+    public String sonarQubeTest(){
+        var a = 1;
+        var b = 2;
+        var c = "sonarQubeTest";
+        System.out.println(c);
+        return c;
+    }
 }
