24c0f1bfa2fcba30eef48f6489c3db8d544adf2e
gaugerard <gauthier.gerard@student.unamur.be>
2022-12-11 23:08:39 +0100

    BROS-35: test

--- integration/src/test/java/game/IntegrationTestGame.java
+++ integration/src/test/java/game/IntegrationTestGame.java
@@ -28,7 +28,7 @@ import static org.awaitility.Awaitility.await;
 @SpringBootTest(classes = Main.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @ActiveProfiles("dev")
-class IntegrationTest {
+class IntegrationTestGame {
 
     @Autowired
     private GamesController gamesController;
--- integration/src/test/java/nerima/IntegrationTestNerima.java
+++ integration/src/test/java/nerima/IntegrationTestNerima.java
@@ -23,7 +23,7 @@ import static org.awaitility.Awaitility.await;
 @SpringBootTest(classes = Main.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @ActiveProfiles("dev")
-class IntegrationTest {
+class IntegrationTestNerima {
 
     @Autowired
     private NerimaController nerimaController;
