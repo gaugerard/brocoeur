8b9cbb2976888bf424051dfd1ea88d242ba05b2b
gaugerard <gauthier.gerard@student.unamur.be>
2022-12-25 19:13:03 +0100

    BROS-42 : Added Bean for 'DeckOfCards' in Games.Main config

--- games/src/main/java/brocoeur/example/games/Main.java
+++ games/src/main/java/brocoeur/example/games/Main.java
@@ -1,11 +1,18 @@
 package brocoeur.example.games;
 
+import brocoeur.example.common.DeckOfCards;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class,args);
+    }
+
+    @Bean
+    public DeckOfCards deckOfCards() {
+        return new DeckOfCards();
     }
 }
